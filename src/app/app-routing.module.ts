35627cb949e371aac1ba6f31c3603f2cda0f5151
Bhagyashri <bpp920797@tatatechnologies.com>
2022-08-26 12:41:24 +0530

    Structural Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CncDataComponent } from './cnc-data/cnc-data.component';
-import { CncliveComponent } from './cnclive/cnclive.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { CncDataComponent } from './components/cnc-data/cnc-data.component';
+import { CncliveComponent } from './components/cnclive/cnclive.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { CardComponent } from './card/card.component';
-import { CncDataComponent } from './cnc-data/cnc-data.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { CardComponent } from './components/card/card.component';
+import { CncDataComponent } from './components/cnc-data/cnc-data.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CncliveComponent } from './cnclive/cnclive.component';
+import { CncliveComponent } from './components/cnclive/cnclive.component';
 
 
 @NgModule({
