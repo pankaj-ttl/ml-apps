6e59aec3e03f43d724eac75af93db13174ff425e
Bhagyashri <bpp920797@tatatechnologies.com>
2022-08-30 11:23:44 +0530

    Ready to setup in workstation

--- src/app/components/cnclive/cnclive.component.ts
+++ src/app/components/cnclive/cnclive.component.ts
@@ -8,6 +8,11 @@ import { HttpClient} from '@angular/common/http';
   styleUrls: ['./cnclive.component.scss']
 })
 export class CncliveComponent implements OnInit {
+  
+  baseurl = "http://172.28.77.122:2000/";
+  url1 = "";
+  model_id=1;
+
    
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -30,11 +35,28 @@ onRemove(event: File) {
 
 onUpload(){
   const filedata=new FormData();
-  filedata.append('id',this.files[0]);
-  this.http.post('URL',filedata)
+  filedata.append('file',this.files[0]);
+  filedata.append('model','2');
+  let response: any[] = []
+  console.log(this.model_id)
+  this.http.post(`${ this.baseurl}upload-csv`,filedata)
   .subscribe(res=>{
     console.log(res);
+    response.push(res);
+    console.log(response);
+    let bar_url = ""
+    let hist_url = ""
+     response.forEach(e => { bar_url = e.plot_urls[0]
+                           hist_url = e.plot_urls[1]})
+    
+                           console.log(bar_url)
+    console.log(hist_url)
+     this.url1 = this.baseurl + bar_url;
+    let url2 = this.baseurl + hist_url;
+    
+    console.log(this.url1);
   })
+  
 }
 
 }
